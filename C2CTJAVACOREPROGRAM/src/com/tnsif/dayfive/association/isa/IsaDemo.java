61d5f25a7f0ec53cd659e0909970c22df36dde7a
ayrawhsi2004 <22eee009@act.edu.in>
2025-06-26 10:23:17 +0530

    isa

---
+++ C2CTJAVACOREPROGRAM/src/com/tnsif/dayfive/association/isa/IsaDemo.java
@@ -0,0 +1,25 @@
+package com.tnsif.dayfive.association.isa;
+
+public class IsaDemo {
+public static void main(String[] args) {
+	Manager m= new Manager ("aishwarya",17,"EEE",90);
+	Employee e=new Employee("jenifer",26,"IT");
+	System.out.println(e);
+	System.out.println(m);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
