0252e2243678390336dbe3a6650b2d6041721257
ayrawhsi2004 <22eee009@act.edu.in>
2025-06-26 10:07:48 +0530

    HASA

---
+++ C2CTJAVACOREPROGRAM/src/com/tnsif/dayfive/associated/hasa/Executor.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayfive.associated.hasa;
+
+public class Executor {
+public static void main(String[] args) {
+Address address=new Address("405 laxmi Enclave,Pandit colony","Nashik","Maharashtra","244567");
+Person person= new Person("Aniket pawar",address);
+
+person.displayInfo();
+
+
+
+
+
+}
+
+}
