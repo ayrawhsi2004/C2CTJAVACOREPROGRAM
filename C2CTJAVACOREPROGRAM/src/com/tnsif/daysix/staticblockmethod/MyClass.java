b4f1b8234f8033b099ac1ef7254ccd5994b99317
ayrawhsi2004 <22eee009@act.edu.in>
2025-06-30 10:37:30 +0530

    Staticblockmethod

---
+++ C2CTJAVACOREPROGRAM/src/com/tnsif/daysix/staticblockmethod/MyClass.java
@@ -0,0 +1,9 @@
+package com.tnsif.daysix.staticblockmethod;
+
+public class MyClass {
+
+	public MyClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ C2CTJAVACOREPROGRAM/src/com/tnsif/daysix/staticblockmethod/MyClassDemo.java
@@ -0,0 +1,9 @@
+package com.tnsif.daysix.staticblockmethod;
+
+public class MyClassDemo {
+
+	public MyClassDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
